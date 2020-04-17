41c5af5f9eb76baf1a84318582c6e14278a3f231
yuefunglee <yuefunglee@cdnis.edu.hk>
2020-04-17 09:05:49 +0800

    Third Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,22 @@
 package com.company;
-
 import java.util.Stack;
 
 public class Main {
 
+    public static Stack cardStack = new Stack();
+
     public static void main(String[] args) {
 
-        Stack cardStack = new Stack();
-        for (int i = 0; i < Card.suit; i++) {
-            for (int j = 0; j < card.length; j++) {
-                cardStack.push(
+        char[] suits = {'D','C','H','S'};
+        String cards[] = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                cardStack.push(cards[i] + " of " + suits[j]);
             }
         }
 
-        String peekStack = (String) cardStack.peek();
-        System.out.println(cardStack);
+        System.out.println(cardStack.peek());
 
     }
 
